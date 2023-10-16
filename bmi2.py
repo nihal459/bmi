4a01006b185d05dbc2781eb673137ec62df1596f
nihal459 <nihalonline24@gmail.com>
2023-10-16 12:12:06 +0530

    abcd

--- bmi2.py
+++ bmi2.py
@@ -1,17 +1,26 @@
+def calcu(w,h):
+    heightinmeters = h/100
+    print("Your height in meters", heightinmeters)
+    newheight = heightinmeters*heightinmeters
+    bmi = w/newheight
+    return bmi
+
+def bmi_value(bmi):
+    if bmi<18.5:
+        print("Underweight")
+    elif bmi >=18.5 and bmi <=24.9:
+        print("Normalweight")
+    elif bmi >=24.9 and bmi <=29.9:
+        print("overweight")
+    else:
+        print("obese")
+
 print("BMI Calculator")
 weight = int(input("Enter your weight in kg"))
 height = int(input("Enter your weight in cm"))
-height2 = height/100
-print("Your height in meters", height2)
-height3 = height2*height2
-bmi = weight/height3
-print("Your BMI is: ", bmi) 
-if bmi<18.5:
-    print("Underweight")
-elif bmi >=18.5 and bmi <=24.9:
-    print("Normalweight")
-elif bmi >=24.9 and bmi <=29.9:
-    print("overweight")
-else:
-    print("obese")
+bmi = calcu(weight,height)
+print(bmi)
+bmi_value(bmi)
+
+
 
